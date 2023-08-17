6baf6c346aed33c58a173e5101436da52ed19aff
cedric <cedric.sillaber@gmail.com>
2023-08-17 22:57:00 +0200

    further impl

--- main.cpp
+++ main.cpp
@@ -4,14 +4,45 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include "main.h"
+
+#define BOARD_WIDTH 10
+#define BOARD_HEIGHT 20
+
+int ch;
+int gameBoard[BOARD_HEIGHT][BOARD_WIDTH]; // 2D array to represent the game board
 
 
 int main (int argc, char* argv[])
 {
     initscr();
-    WINDOW* win = newwin(100, 100, 40, 30 );
+    noecho();
+    resize_term(22, 22);
+    timeout(0);
+    curs_set(0);
+    box(stdscr, 0, 0);
 
-    sleep(10);
-
+    main_loop();
     endwin();
 }
+
+/*  ------------------------------  */
+
+void main_loop()
+{
+    while (!hit_bottom())
+    {
+        usleep(10000);
+        ch = getchar();
+        if (ch == 'q')
+        {
+            break;
+        }
+    }
+}
+
+int hit_bottom()
+{
+    return 0;
+}
+
--- main.h
+++ main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void main_loop(void);
+void main_loop();
+int hit_bottom();
 
 #endif
