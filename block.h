e9e7df4129c5eea2edfc6655ae3ebeec0bc7a423
cedric <cedric.sillaber@gmail.com>
2023-08-18 11:06:07 +0200

    add files

---
+++ block.h
@@ -0,0 +1,31 @@
+#ifndef BLOCK_H
+#define BLOCK_H
+
+const int TETROMINO_SIZE = 4;
+
+const int TETROMINO_I[TETROMINO_SIZE][TETROMINO_SIZE] = {
+        {0,0,0,0},
+        {1, 1, 1, 1},
+        {0,0,0,0},
+        {0,0,0,0}
+};
+
+
+const int TETROMINO_J[TETROMINO_SIZE][TETROMINO_SIZE] = {
+        {1,0,0,0},
+        {1, 1, 1, 0},
+        {0,0,0,0},
+        {0,0,0,0}
+};
+
+const int TETROMINO_L[TETROMINO_SIZE][TETROMINO_SIZE] = {
+        {1,0,0,0},
+        {1, 1, 1, 0},
+        {0,0,0,0},
+        {0,0,0,0}
+};
+
+// add more
+
+
+#endif
--- main.h
+++ main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#define BOARD_WIDTH 50
+#define BOARD_HEIGHT 50
+#define EMPTY_CELL 0
+
 void main_loop();
 int hit_bottom();
 void init_colors();
